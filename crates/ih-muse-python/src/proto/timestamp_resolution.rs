ca1d0f9b41321a809a130f2f033d8d3c31c6c467
codelluis <165762916+codelluis@users.noreply.github.com>
2024-12-12 09:38:45 +0100

    feat(python): add to_timedelta method to TimestampResolution and tests (#23)
    * feat: adding get finest resolution to python Muse
    
    * feat: release 0.0.22
    
    * feat(python): add to_timedelta method to TimestampResolution and tests

--- crates/ih-muse-python/src/proto/timestamp_resolution.rs
+++ crates/ih-muse-python/src/proto/timestamp_resolution.rs
@@ -1,4 +1,7 @@
+// crates/ih-muse-python/src/proto/timestamp_resolution.rs
+
 use pyo3::prelude::*;
+use pyo3::types::PyDelta;
 
 use ih_muse_proto::TimestampResolution as RustTimestampResolution;
 
@@ -14,6 +17,30 @@ pub enum PyTimestampResolution {
     Seconds,
     Milliseconds,
     Microseconds,
+}
+
+#[pymethods]
+impl PyTimestampResolution {
+    fn to_timedelta(&self, py: Python) -> PyResult<PyObject> {
+        let rust_resolution: RustTimestampResolution = self.clone().into();
+        let duration = rust_resolution.to_duration();
+
+        let total_seconds = duration.as_secs();
+        let microseconds = duration.as_micros() % 1_000_000;
+
+        // Create a &PyDelta reference
+        let delta_ref = PyDelta::new_bound(
+            py,
+            0,                    // days
+            total_seconds as i32, // seconds
+            microseconds as i32,  // microseconds
+            true,                 // normalize
+        )?;
+
+        // Convert &PyDelta to PyObject
+        // This PyObject is a Python datetime.timedelta object
+        Ok(delta_ref.to_object(py))
+    }
 }
 
 impl From<RustTimestampResolution> for PyTimestampResolution {
